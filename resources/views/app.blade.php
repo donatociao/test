018066b97bb58ba634bae81dfe31098804c687e0
Donato <ciao.donatociao@gmail.com>
2020-03-27 12:57:30 +0100

    Google Tag Manager prelibato.eu

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,26 +4,9 @@
       @include('partials.head')
     </head>
     <body>
-      <script>
-      window.fbAsyncInit = function() {
-        FB.init({
-          appId            : '671623543590916',
-          autoLogAppEvents : true,
-          xfbml            : true,
-          version          : 'v6.0'
-        });
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
-      };
-    </script>
-    <script async defer src="https://connect.facebook.net/en_US/sdk.js"></script>
       <!-- Google Tag Manager (noscript) -->
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5GZPVWG" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MV3KZB4"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
       <!-- End Google Tag Manager (noscript) -->
 
       @yield('content')
