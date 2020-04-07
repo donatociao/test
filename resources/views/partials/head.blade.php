c08ca3992fd699ddd211a4362c18448847201481
Donato <ciao.donatociao@gmail.com>
2020-04-07 20:36:35 +0200

    Re cdns

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -90,7 +90,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 }
 </script>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="{{asset('lib/jquery/jquery.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('lib/jquery.ihavecookies.min.js')}}"></script>
 
  <script type="text/javascript">
--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -3,17 +3,17 @@
 <div id="preloader"></div>
 
 <!-- JS Libs -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/3.1.0/jquery-migrate.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"
+<script src="{{asset('lib/jquery/jquery-migrate.min.js')}}"></script>
+<script src="{{asset('js/jquery-ui.min.js')}}"
 integrity="sha256-eGE6blurk5sHj+rmkfsGYeKyZx3M4bG+ZlFyA7Kns7E="
 crossorigin="anonymous"></script>
 <script src="{{asset('lib/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
+<script src="{{asset('lib/wow/wow.min.js')}}"></script>
 <script src="{{asset('js/wavify.min.js')}}"></script>
 <script src="{{asset('js/jquery.wavify.js')}}"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js"></script>
+<script src="{{asset('js/anime.min.js')}}"></script>
+<script src="{{asset('lib/jquery.waypoints.min.js')}}"></script>
+<script src="{{asset('js/tweenmax.min.js')}}"></script>
 
 <!-- main js -->
 <script src="{{asset('js/app.min.js')}}"></script>
