a2f517310c3a34836c99ea889d7daea893e0a832
Donato <ciao.donatociao@gmail.com>
2020-12-09 03:25:09 +0100

    sitemap Up

--- resources/views/partials/cosafacciamo.blade.php
+++ resources/views/partials/cosafacciamo.blade.php
@@ -12,54 +12,64 @@
     </header>
     <div class="row st-row">
       <div class="offset-lg-1 offset-md-1 offset-sm-1 col-md-5 col-lg-5 wow zoomIn" data-wow-duration="1.4s">
-        <div class="box" id="boxuno">
-          <h3 class="title">VISUAL</h3>
-          <ul>
-            <li>Logo & Branding</li>
-            <li>UI / UX Design</li>
-            <li>Packaging</li>
-            <li>Editoria</li>
-            <li>Foto & Video</li>
-            <li>Stand & Interior Design</li>
-          </ul>
-        </div>
+        <a href="#">
+          <div class="box" id="boxuno">
+            <h3 class="title">VISUAL</h3>
+            <ul>
+              <li>Logo & Branding</li>
+              <li>UI / UX Design</li>
+              <li>Food Packaging</li>
+              <li>Grafica editoriale</li>
+              <li>Foto & Video</li>
+              <li>Retail Design</li>
+            </ul>
+          </div>
+        </a>
       </div>
       <div class="offset-lg-1 offset-md-1 offset-sm-1  col-md-5 col-lg-5 wow zoomIn" data-wow-duration="1.4s">
-        <div class="box">
-          <h3 class="title">WEB</h3>
-          <ul>
-            <li>Siti Web Responsive</li>
-            <li>Sviluppo Front / Back-end</li>
-            <li>Applicazioni Web</li>
-            <li>e-Commerce</li>
-            <li>Landing Pages</li>
-            <li>Blog</li>
-          </ul>
-        </div>
+        <a href="#">
+          <div class="box">
+            <h3 class="title">WEB</h3>
+            <ul>
+              <li>Siti Web Responsive</li>
+              <li>Sviluppo Front / Back-end</li>
+              <li>Applicazioni Web</li>
+              <li>e-Commerce</li>
+              <li>Landing Pages</li>
+              <li>Blog</li>
+            </ul>
+          </div>
+        </a>
       </div>
     </div>
     <div class="row">
       <div class="offset-lg-1 offset-md-1 offset-sm-1 col-md-5 col-lg-5 wow zoomIn" data-wow-duration="1.4s">
-        <div class="box">
+        <a href="{{route('digital')}}">
+         <div class="box">
           <h3 class="title">DIGITAL</h3>
           <ul>
             <li>Strategy & Advertising</li>
             <li>Copywriting</li>
             <li>Social Media Marketing</li>
             <li>Email Marketing</li>
-            <li>SEO & SEM</li>
+            <li>Consulenza SEO</li>
+            <li>Google Ads</li>
           </ul>
         </div>
+       </a>
       </div>
       <div class="offset-lg-1 offset-md-1 offset-sm-1  col-md-5 col-lg-5 wow zoomIn" data-wow-duration="1.4s">
-        <div class="box">
-          <h3 class="title">EVENTI</h3>
-          <ul>
-            <li>Progettazione</li>
-            <li>Planning & Logistica</li>
-            <li>Promozione online / offline</li>
-          </ul>
-        </div>
+        <a href="#">
+          <div class="box">
+            <h3 class="title">EVENTI</h3>
+            <ul>
+              <li>Progettazione</li>
+              <li>Planning & Logistica</li>
+              <li>Promozione online / offline</li>
+              <li>Gadgets</li>
+            </ul>
+          </div>
+        </a>
       </div>
     </div>
   </div>
