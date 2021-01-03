9a325e38ab6601ac0d0b8887a357c4157bbe576b
Donato <ciao.donatociao@gmail.com>
2021-01-03 01:43:21 +0100

    Hey oliver ok

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,18 +13,7 @@
 
       @yield('content')
 
-      <script type='text/javascript'>
-      var _hoid = _hoid || []; _hoid.push('ho_NzpZa6vjVxe4YkSAgR1P5MtmWyJUdfXT7sDFqQ8bcC0wE2n');
-      var heyopath = (('https:' == document.location.protocol) ? 'https://www.heyoliver.com/webroot/ho-ui/v2/' :
-      'http://www.heyoliver.com/webroot/ho-ui/v2/');
-      var heyop = (('https:' == document.location.protocol) ? 'https://' : 'http://');
-      var heyospt = document.createElement('script'); heyospt.type = 'text/javascript';
-      heyospt.async = true; heyospt.src = heyopath + 'ho2.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(heyospt, s);
-      </script>
-  <!-- End of Hey Oliver  -->
       @include('partials.scripts')
-        <p class="popupBubbly text-center">click here</p>
-      <!-- Hey Oliver -->
+
     </body>
 </html>
