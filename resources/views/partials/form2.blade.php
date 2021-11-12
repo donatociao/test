b3e626e131a96f7e39d6390c4fd791fe8dab5761
Donato <ciao.donatociao@gmail.com>
2021-11-12 14:16:29 +0100

    form ok
    Form ok

---
+++ resources/views/partials/form2.blade.php
@@ -0,0 +1,133 @@
+  <!-- Footer -->
+<svg id="svganime" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 391.06 18.1">
+  <style>.cls-1{fill:#fce76d;}</style>
+  <path id="footerwave" class="cls-1" d="M391.06,0c-48.88,0-48.88,11.34-97.77,11.34S244.41,0,195.52,0,146.64,11.34,97.76,11.34,48.88,0,0,0V18.1H391.06Z"/>
+</svg>
+<footer id="footer" class="section-bg">
+  <div class="footer-top">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-5">
+          <div class="footer-links mb-5">
+            <h3 class="contattaci">Parliamo di futuro.</h3>
+            <p class="form-contact">
+              <b>Prelibato srl</b><br>Via S. Giovanni, 66<br>
+              <span>84025 Eboli, SA - Italia<span><br><br>
+              <b><i class="fa fa-phone"></i></b> <a href="callto:+3908281953233">+39 0828 19 53 233</a><br>
+              <b><i class="fa fa-envelope-o"></i></b> info@prelibato.eu<br>
+            </p>
+            <div class="social-links">
+              <a target="_blank" rel="noopener noreferrer" title="Visita la pagina Facebook" href="http://www.facebook.com/prelibatoFoodMarketing" class="facebook"><i class="fa fa-facebook"></i></a>
+              <a target="_blank" rel="noopener noreferrer" title="Seguici su Instagram" href="https://www.instagram.com/_prelibato_/" class="instagram"><i class="fa fa-instagram"></i></a>
+              <a target="_blank" rel="noopener noreferrer" title="Seguici su LinkedIn" href="https://www.linkedin.com/company/prelibatosrl " class="linkedin"><i class="fa fa-linkedin"></i></a>
+              <a target="_blank" title="Seguici su Behance!" rel="noopener noreferrer" href="https://www.behance.net/prelibato" class="linkedin"><i class="fa fa-behance"></i></a>
+              <a target="_blank" title="Hai bisogno di aiuto? Scrivici una mail!" rel="noopener noreferrer" href="mailto:info@prelibato.eu" class="linkedin"><i class="fa fa-envelope-o"></i></a>
+            </div>
+            <div id="fb-root"></div>
+            <script async defer crossorigin="anonymous" src="https://connect.facebook.net/it_IT/sdk.js#xfbml=1&version=v6.0&appId=671623543590916&autoLogAppEvents=1"></script>
+            <div class="fb-like mt-5" data-href="https://www.facebook.com/prelibatoFoodMarketing" data-width="" data-layout="button_count" data-action="like" data-size="small" data-share="true"></div>          </div>
+        </div>
+        <div class="offset-lg-1 col-lg-6">
+          <div class="footer-links">
+            <h3 class="contattaci">Contattaci</h3>
+            <div class="col-lg-12 mb-5">
+              {{-- <script src="https://clienti.prelibato.eu/frm/S6QKWauiUGY5kTM.js"></script> --}}
+              <form name="VTEForm_5fd61d7a1907b23c3f4eb6233b7cd6f4" action="http://62.171.164.61/test/modules/Webforms/capture.php" method="post" accept-charset="utf-8">
+              	<p>
+              		<input type="hidden" name="publicid" value="5fd61d7a1907b23c3f4eb6233b7cd6f4" />
+              		<input type="hidden" name="name" value="test" />
+              	</p>
+              									<p>
+              				<label>First Name</label>
+              															<input type="text" value="" name="firstname"  />
+              												</p>
+              											<p>
+              				<label>Last Name</label>
+              															<input type="text" value="" name="lastname" required="true" />
+              												</p>
+              											<p>
+              				<label>Company</label>
+              															<input type="text" value="" name="company" required="true" />
+              												</p>
+              											<p>
+              				<label>Email</label>
+              															<input type="text" value="" name="email"  />
+              												</p>
+              				<p>
+              		<input type="submit" value="Submit" />
+              	</p>
+              </form>
+
+              <script type="text/javascript">
+              (function() {
+              	var M = navigator.userAgent.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+
+              	if(M[1] == 'Safari'){
+              		var vte_key = document.getElementsByName('publicid');
+              		if (!vte_key) return;
+
+              		var forms = document.getElementsByName('VTEForm_'+vte_key[0].value);
+
+              		for (var i = 0; i < forms.length; i++) {
+              			forms[i].noValidate = true;
+              			forms[i].addEventListener('submit', function(event) {
+              				//Prevent submission if checkValidity on the form returns false.
+              				if (!event.target.checkValidity()) {
+              					event.preventDefault();
+              					var inputs = event.target.getElementsByTagName('input');
+
+              					for (var j = 0; j< inputs.length; ++j) {
+              						if (inputs[j].validity && !inputs[j].validity.valid){
+              							var label = inputs[j].previousElementSibling.innerHTML;
+              							alert('Non valido '+label);
+              							return false;
+              						}
+              					}
+              					var selects = event.target.getElementsByTagName('select');
+              					for (var j = 0; j< selects.length; ++j) {
+              						if (selects[j].validity && !selects[j].validity.valid){
+              							var label = selects[j].previousElementSibling.innerHTML;
+              							alert('Non valido '+label);
+              							return false;
+              						}
+              					}
+              					var tarea = event.target.getElementsByTagName('textarea');
+              					for (var j = 0; j< tarea.length; ++j) {
+              						if (tarea[j].validity && !tarea[j].validity.valid){
+              							var label = tarea[j].previousElementSibling.innerHTML;
+              							alert('Non valido '+label);
+              							return false;
+              						}
+              					}
+              				}
+              			}, false);
+              		}
+              	}
+              })();
+              </script>
+
+
+
+
+              {{-- <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/shell.js"></script>
+              <script>
+                hbspt.forms.create({
+              	portalId: "7355049",
+              	formId: "aec6882c-cdd7-472d-b250-7b1a89ae8cca"
+              });
+              </script> --}}
+              </div>
+              </div>
+              </div>
+              </div>
+              </div>
+              </div>
+              </div>
+
+              <div class="bg-prelibato text-center">
+                  <div class="copyright pb-3">
+                      <strong>Prelibato è un'agenzia di food marketing e comunicazione | Prelibato srl - P.IVA 05699060652</strong>. &copy; Copyright {{ now()->year }}. Tutti i diritti riservati. Leggi la nostra <a title="Privacy & Cookie Policy"
+                        href="{{route('privacy')}}">Privacy policy.</a>
+                  </div>
+              </div>
+              </footer><!-- fine footer -->
